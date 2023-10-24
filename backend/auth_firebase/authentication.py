fd6fbba55af33f21654724a156b9947b6ce79763
hyungseok-choi <hsuk1648@gmail.com>
2023-10-25 02:39:58 +0900

    Fix credential for Firebase auth

--- backend/auth_firebase/authentication.py
+++ backend/auth_firebase/authentication.py
@@ -10,15 +10,15 @@ cred = credentials.Certificate({
     "type" : settings.FIREBASE_TYPE,
     "project_id" : settings.FIREBASE_PROJECT_ID,
     "private_key_id" : settings.FIREBASE_PRIVATE_KEY_ID,
-    "private_key" : settings.FIREBASE_PRIVATE_KEY.replace('\\n', '\n'),
+    "private_key" : settings.FIREBASE_PRIVATE_KEY.replace("\\n", "\n"),
     "client_email" : settings.FIREBASE_CLIENT_EMAIL,
     "client_id" : settings.FIREBASE_CLIENT_ID,
     "auth_uri" : settings.FIREBASE_AUTH_URI,
     "token_uri" : settings.FIREBASE_TOKEN_URI,
     "auth_provider_x509_cert_url" : settings.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url" : settings.FIREBASE_CLIENT_X509_CERT_URL
+    "client_x509_cert_url" : settings.FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain" : settings.FIREBASE_UNIVERSE_DOMAIN,
 })
-
 default_app = firebase_admin.initialize_app(cred)
 
 """FIREBASE AUTHENTICATION"""
