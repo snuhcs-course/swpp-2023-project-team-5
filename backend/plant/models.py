46cc823c8cd559e6fb09452e378254ce05d5be34
hyungseok-choi <hsuk1648@gmail.com>
2023-10-25 02:41:28 +0900

    Add plant app with initial settings

--- backend/plant/models.py
+++ backend/plant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    birthday = models.DateField()
+    height = models.IntegerField()
+    status = models.CharField(max_length=100)
+    last_watered = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- backend/plant/views.py
+++ backend/plant/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Plant
+from .serializer import PlantSerializer
+
+class PlantView(APIView):
+    def get(self, request):
+        plants = Plant.objects.all()
+        serializer = PlantSerializer(plants, many=True)
+        return Response({"plants": serializer.data})
