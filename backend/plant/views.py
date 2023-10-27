6514d1e2021e1018be2ea029acf86bf9fc91e0a4
hyungseok-choi <hsuk1648@gmail.com>
2023-10-28 02:48:25 +0900

    Add POST endpoint for plant

--- backend/plant/views.py
+++ backend/plant/views.py
@@ -5,6 +5,24 @@ from django.contrib.auth.models import User
 from .models import Plant
 from .serializer import PlantSerializer
 
+class PlantBasic(APIView):
+    def post(self, request):
+        user_id = request.data.get('user_id', None)
+        
+        # Check if the user with the provided user_id exists
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({'user_id': 'User does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = PlantSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"plant": serializer.data})
+        else:
+            return Response({"plant": serializer.errors})
+
+
 class PlantUser(APIView):
     def get(self, request, user_id):
         plants = Plant.objects.filter(user_id=user_id)
